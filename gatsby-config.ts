2d9891fe2508de7705e5851841a2ad0a2ece4bb4
Becca <123378408+hayeonrjoe@users.noreply.github.com>
2025-01-16 02:38:25 +0900

    Initial commit: Customized Gatsby starter template for my personal website

---
+++ gatsby-config.ts
@@ -0,0 +1,78 @@
+import type { GatsbyConfig, PluginRef } from "gatsby"
+import "dotenv/config"
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+const config: GatsbyConfig = {
+  pathPrefix: "/hayeonrjoe.github.io",
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.mjs
+    siteTitle: `Hayeon R Joe`,
+    siteTitleAlt: `Hayeon R Joe`,
+    siteHeadline: `Hayeon R Joe`,
+    siteUrl: `https://hayeonrjoe.github.io`,
+    siteDescription: `Hayeon R Joe's personal website`,
+    siteImage: `/banner.jpg`,
+    siteLanguage: `en`,
+    author: `@hayeonrjoe`,
+  },
+  trailingSlash: `always`,
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-jodie`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          { name: `Projects`, slug: `/projects` },
+          { name: `Research`, slug: `/research` },
+          { name: `About`, slug: `/about` },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Hayeon R Joe`,
+        short_name: `Hayeon R Joe`,
+        description: `Hayeon R Joe's personal website`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#b75e09`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    // You can remove this plugin if you don't need it
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-statoscope`,
+      options: {
+        saveReportTo: `${__dirname}/public/.statoscope/_bundle.html`,
+        saveStatsTo: `${__dirname}/public/.statoscope/_stats.json`,
+        open: false,
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+}
+
+export default config
